095e7450d4f609d593184611d3e31df3fcb2c51a
Thomas Eding <thomaseding@gmail.com>
2015-06-26 21:25:01 -0700

    initial

---
+++ src/AnsiToPrompt.hs
@@ -0,0 +1,34 @@
+module AnsiToPrompt (
+      main
+    ) where
+
+
+import Control.Monad.ListM
+import Control.Monad.State.Strict
+import Data.Char
+import System.IO
+
+
+main :: IO ()
+main = interact (flip evalState Normal . ansiToPrompt)
+
+
+data Status = Normal | Escaping
+
+
+ansiToPrompt :: String -> State Status String
+ansiToPrompt str = case str of
+    '\ESC' : rest -> do
+        put Escaping
+        liftM ("\\[\ESC" ++) $ ansiToPrompt rest
+    'm' : rest -> do
+        status <- get
+        case status of
+            Normal -> liftM ('m' :) $ ansiToPrompt rest
+            Escaping -> do
+                put Normal
+                liftM ("m\\]" ++) $ ansiToPrompt rest
+    c : rest -> liftM (c :) $ ansiToPrompt rest
+    "" -> return ""
+
+
---
+++ src/Main.hs
@@ -0,0 +1,12 @@
+module Main (
+      main
+    ) where
+
+
+import qualified AnsiToPrompt
+
+
+main :: IO ()
+main = AnsiToPrompt.main
+
+
